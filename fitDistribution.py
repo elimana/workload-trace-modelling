6267346f7cd078ccb3f1f4fc996a39f7e1430ff0
Eli Mana <elimana97@gmail.com>
2021-06-28 14:30:30 +1000

    Add PDF generation function

--- fitDistribution.py
+++ fitDistribution.py
@@ -73,3 +73,22 @@ def best_fit_distribution(data, bins=200, ax=None):
       pass
 
   return (best_distribution.name, best_params)
+
+def make_pdf(dist, params, size=10000):
+  """Generate distributions's Probability Distribution Function """
+
+  # Separate parts of parameters
+  arg = params[:-2]
+  loc = params[-2]
+  scale = params[-1]
+
+  # Get sane start and end points of distribution
+  start = dist.ppf(0.01, *arg, loc=loc, scale=scale) if arg else dist.ppf(0.01, loc=loc, scale=scale)
+  end = dist.ppf(0.99, *arg, loc=loc, scale=scale) if arg else dist.ppf(0.99, loc=loc, scale=scale)
+
+  # Build PDF and turn into pandas Series
+  x = np.linspace(start, end, size)
+  y = dist.pdf(x, loc=loc, scale=scale, *arg)
+  pdf = pd.Series(y, x)
+
+  return pdf
